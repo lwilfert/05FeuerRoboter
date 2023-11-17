52f2071ed92fbe5c7549b7d880d2b06fd128c4f5
iM0ritz <moritz.schlitter@kabelmail.de>
2023-11-17 11:42:50 +0100

    add basic_drive_test

---
+++ Moritz_CamTest/basic_drive_test.py
@@ -0,0 +1,58 @@
+from time import sleep
+
+import cv2
+import numpy as np
+import requests
+import pyrealsense2 as rs
+from PIL import Image
+
+URI = "http://127.0.0.1:5000"
+
+
+def send_right_request():
+    url = f"{URI}/right"
+    body = {"value": 80}
+
+    response = requests.post(url, json=body)
+    print(response.text)
+
+    # sleep(3)
+    # stop_control_request()
+
+
+def send_left_request():
+    url = f"{URI}/right"
+    body = {"value": 100}
+
+    response = requests.post(url, json=body)
+    print(response.text)
+
+
+def send_center_request():
+    url = f"{URI}/right"
+    body = {"value": 90}
+
+    response = requests.post(url, json=body)
+    print(response.text)
+
+
+def send_start_request():
+    url = f"{URI}/go"
+    body = {"value": 0.12}
+
+    response = requests.post(url, json=body)
+    print(response.text)
+
+
+def send_stop_request():
+    url = f"{URI}/stop"
+
+    response = requests.post(url)
+    print(response.text)
+
+
+send_start_request()
+send_left_request()
+send_center_request()
+send_right_request()
+send_stop_request()
