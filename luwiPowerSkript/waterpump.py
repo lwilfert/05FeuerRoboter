833f4ca1fc8d1ba49033d1af7edad9e3e604a72c
Katharina Reiner <katharina.reiner@hof-university.de>
2024-01-18 17:57:47 +0100

    refac: use waterpump as component ?

--- luwiPowerSkript/waterpump.py
+++ luwiPowerSkript/waterpump.py
@@ -14,3 +14,18 @@ class WaterPump:
         os.popen(off_path)
         print("stopped pumping")
 
+
+# TODO: try if this works as component or why did i never include this?
+# from katy_mainControl.abstract_component import Component
+#
+#
+# class WaterPump(Component):
+#     def get_target(self):
+#         return self.start_pumping_water
+#
+#     def start_pumping_water(self):
+#         os.popen(on_path)
+#
+#     def stop(self):
+#         os.popen(off_path)
+#         super.stop()
